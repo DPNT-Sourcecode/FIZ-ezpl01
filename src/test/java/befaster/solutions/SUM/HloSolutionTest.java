a301d542f3da0abaa8a127f69a4f423f332a3ff1
sbx_user1060 <sbx_user1060@ip-10-128-216-24.eu-west-2.compute.internal>
2018-10-02 20:34:14 +0000

    Tue Oct 02 20:34:14 UTC 2018

--- src/test/java/befaster/solutions/SUM/HloSolutionTest.java
+++ src/test/java/befaster/solutions/SUM/HloSolutionTest.java
@@ -1,4 +1,12 @@
 package befaster.solutions.SUM;
 
+import befaster.solutions.HLO.HelloSolution;
+import org.junit.Test;
+
 public class HloSolutionTest {
+    @Test
+    public void helloTest() throws Exception {
+        HelloSolution helloSolution = new HelloSolution();
+        String feedback = helloSolution.hello("Friend");
+    }
 }

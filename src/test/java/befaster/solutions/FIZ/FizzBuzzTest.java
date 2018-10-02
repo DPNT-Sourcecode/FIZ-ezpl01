8e5bc079aaeaa30959e2202bf17a7ae8385244ef
sbx_user1060 <sbx_user1060@ip-10-128-216-24.eu-west-2.compute.internal>
2018-10-02 20:48:28 +0000

    Tue Oct 02 20:48:28 UTC 2018

---
+++ src/test/java/befaster/solutions/FIZ/FizzBuzzTest.java
@@ -0,0 +1,17 @@
+package befaster.solutions.FIZ;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class FizzBuzzTest {
+    @Test
+    public void testFizz() throws Exception {
+        FizzBuzzSolution solution = new FizzBuzzSolution();
+        Assert.assertEquals("fizz", solution.fizzBuzz(3));
+    }
+    @Test
+    public void testBuzz() throws Exception {
+        FizzBuzzSolution solution = new FizzBuzzSolution();
+        Assert.assertEquals("buzz", solution.fizzBuzz(5));
+    }
+}

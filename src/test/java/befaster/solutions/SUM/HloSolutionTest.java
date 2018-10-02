3ed6162de3fdcb725987ee0dac4a906f1c8cea20
sbx_user1060 <sbx_user1060@ip-10-128-216-24.eu-west-2.compute.internal>
2018-10-02 20:41:43 +0000

    Tue Oct 02 20:41:43 UTC 2018

--- src/test/java/befaster/solutions/SUM/HloSolutionTest.java
+++ src/test/java/befaster/solutions/SUM/HloSolutionTest.java
@@ -8,7 +8,8 @@ public class HloSolutionTest {
     @Test
     public void helloTest() throws Exception {
         HelloSolution helloSolution = new HelloSolution();
-        String feedback = helloSolution.hello("Friend");
-        Assert.assertEquals("Hello, World!", feedback);
+        String param = "Michael";
+        String feedback = helloSolution.hello(param);
+        Assert.assertEquals("Hello, "+ param +"!", feedback);
     }
 }
